817942123bed644bc280f9ad42c7ee830208c137
mallhar-karkhanis <138213901+mallhar-karkhanis@users.noreply.github.com>
2024-11-16 09:20:15 +0530

    Update Testimonials.jsx

--- src/components/Testimonial/Testimonials.jsx
+++ src/components/Testimonial/Testimonials.jsx
@@ -1,42 +1,49 @@
-import React from 'react';
-import rev1 from  "../../components/Testimonial/rev2.jpeg";
-import rev2 from "../../components/Testimonial/rev3.jpeg";
-import rev3 from "../../components/Testimonial/rev4.jpeg";
-import rev4 from "../../components/Testimonial/rev5.png";
-import rev5 from "../../components/Testimonial/rev6.png";
+ import React from 'react';
+import rev1 from "../../components/Testimonials/rev2.jpeg";
+import rev2 from "../../components/Testimonials/rev3.jpeg";
+import rev3 from "../../components/Testimonials/rev4.jpeg";
+import rev4 from "../../components/Testimonials/rev5.png";
+import rev5 from "../../components/Testimonials/rev6.png";
 
 const Testimonials = () => {
   const testimonialsData = [
     {
       imageSrc: rev2,
-      
     },
     {
       imageSrc: rev4,
-      
     },
     {
       imageSrc: rev5,
-      
     },
   ];
 
   return (
     <section className="bg-gray-100 py-16">
       <div className="container mx-auto px-4">
-        <h2 className="text-3xl font-bold text-center mb-16">Testimonials</h2>
+        <h2 className="text-3xl font-bold text-center mb-16">Testimonials</h2>  
+
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
           {testimonialsData.map((testimonial, index) => (
-            <div key={index} className="bg-white p-8 rounded-lg shadow-md">
-              <div className="flex items-center mb-4">
-                <img src={testimonial.imageSrc} alt="Image Description" className="h-full w-full object-cover rounded" />  
+            <div
+              key={index} 
+
+              className={`bg-white  rounded-lg shadow-md transition-transform duration-300 ease-in-out hover:scale-110`}
+            >
+              <div className="flex items-center ">
+                <img
+                  src={testimonial.imageSrc}
+                  alt="Image Description"
+                  className="h-full w-full object-cover rounded"
+                />
               </div>
               
             </div>
           ))}
         </div>
       </div>
-    </section>
+    </section> 
+
   );
 };
 
